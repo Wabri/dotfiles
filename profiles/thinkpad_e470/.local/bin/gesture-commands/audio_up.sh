b476dbea1c0f8751b373287a7a8015412ec3ed89
Wabri <gabriele.puliti@proton.me>
2022-10-18 08:45:14 +0200

    add gesture binary

---
+++ profiles/thinkpad_e470/.local/bin/gesture-commands/audio_up.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+amixer sset Master 5%+
+notify-send --expire-time=600 --urgency=normal "墳 "`awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master)`
+play ~/.config/i3/tick.mp3
+
