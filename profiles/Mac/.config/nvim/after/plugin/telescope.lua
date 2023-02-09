661298378b005cd8228433086a3009603c96420f
Wabri <12409541+Wabri@users.noreply.github.com>
2023-02-09 21:08:47 +0100

    Add more telescopic jon

--- profiles/Mac/.config/nvim/after/plugin/telescope.lua
+++ profiles/Mac/.config/nvim/after/plugin/telescope.lua
@@ -5,14 +5,20 @@ require("which-key").register({
         name = "Telescope",
         f = { "Find Files"},
         s = { "Search string"},
+        S = { "Live search string"},
         g = { "Find Git Files"},
+        d = { "Diagnostics"},
+        c = { "Treesitter"},
     },
 }, { prefix = "<leader>" })
 -- FILES
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fS', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 -- GIT
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-
+-- LSP
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>fc', builtin.treesitter, {})
