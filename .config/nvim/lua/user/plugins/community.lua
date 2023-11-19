395e7d718aee0d131dc18ee874bc8f93d9776518
Wabri <12409541+Wabri@users.noreply.github.com>
2023-11-20 00:10:56 +0100

    update(nvim): HARPOOOOOOOOON

--- .config/nvim/lua/user/plugins/community.lua
+++ .config/nvim/lua/user/plugins/community.lua
@@ -3,12 +3,12 @@ return {
   -- Colorschemes
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- File explorer
-  { import = "astrocommunity.git.neogit" },
-  -- File explorer
   { import = "astrocommunity.file-explorer.oil-nvim" },
   -- Languages
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   -- Motions
-  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.vim-matchup" },
 }
